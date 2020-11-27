7dff4782dbf730426b9935e46062f02df4e8434c
gulcin-ozkahya <g.ozkahya@ug.bilkent.edu.tr>
2020-11-27 14:45:37 +0300

    first commit

--- app/src/main/java/com/example/kitchapp/MainActivity.java
+++ app/src/main/java/com/example/kitchapp/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Hello World!!!");
         //Hey There Emre
+
+        // code code code
     }
 }
