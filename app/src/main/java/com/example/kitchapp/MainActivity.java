a764bbc43b8ccdad0beb552cdc4e28fe5ef6fa3b
AliGenis <72208412+AliGenis@users.noreply.github.com>
2020-11-27 13:29:58 +0300

    Some androis studio things

--- app/src/main/java/com/example/kitchapp/MainActivity.java
+++ app/src/main/java/com/example/kitchapp/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.pritnln("Hello World");
+        System.out.println("Hello World!!!");
         //Hey There Emre
     }
 }
