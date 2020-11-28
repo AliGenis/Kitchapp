07ffd4f4999c185c4a36fbec34e84e1372e51f35
gulcin-ozkahya <g.ozkahya@ug.bilkent.edu.tr>
2020-11-28 21:59:25 +0300

    Fridge ingredients database
    Co-Authored-By: emreerdal9 <72206462+emreerdal9@users.noreply.github.com>

--- app/src/main/java/com/example/kitchapp/Fridge.java
+++ app/src/main/java/com/example/kitchapp/Fridge.java
@@ -1,4 +1,24 @@
 package com.example.kitchapp;
+import java.util.*;
 
 public class Fridge {
+    public void addByOne(Ingredient ingredient){
+
+    }
+    public void eatByOne(Ingredient ingredient){
+
+    }
+    public ArrayList<Ingredient> ingredientsSpecificToUser(User user){
+
+    }
+    public ArrayList<Recipe> recipesSpecificToUser(User user){
+
+    }
+    public Recipe suggest(User user){
+
+    }
+    public void eatMeal(Recipe recipe){
+
+    }
+
 }
---
+++ app/src/main/java/com/example/kitchapp/Ingredient.java
@@ -0,0 +1,7 @@
+package com.example.kitchapp;
+
+public class Ingredient {
+    private int number;
+    private final int criticalNumber = 3;
+
+}
