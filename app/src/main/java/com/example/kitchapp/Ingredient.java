762b7fb773b028ad11c96816a9db854d23f4c7a4
gulcin-ozkahya <g.ozkahya@ug.bilkent.edu.tr>
2020-12-09 16:07:02 +0300

    Mainly Ingredient class is constructed
    Co-Authored-By: emreerdal9 <72206462+emreerdal9@users.noreply.github.com>

--- app/src/main/java/com/example/kitchapp/Ingredient.java
+++ app/src/main/java/com/example/kitchapp/Ingredient.java
@@ -1,15 +1,45 @@
 package com.example.kitchapp;
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.*;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 public class Ingredient {
     private int number;
-    private final int criticalNumber = 3;
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
-    //myRef.setValue("Hello, World!");
+    private int criticalNumber;
+    private int defaultBuyValue;
+    private String name;
+    private boolean shopable;
+
+    public Ingredient(int number,int criticalNumber,boolean shopable,String name){
+        this.number = number;
+        this.criticalNumber = criticalNumber;
+        this.shopable = shopable;
+        this.name = name;
+    }
+    public Ingredient(int number,boolean shopable,String name){
+        this.number = number;
+        this.shopable = shopable;
+        this.name = name;
+    }
+    /**
+     
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("message");
+
+    DatabaseReference usersRef = ref.child("ingredients");
+    ArrayList<Ingredient> ingr
+   Map<String, Ingredient> ingredients = new HashMap<>();
+
+    ingredients.put("alanisawesome", new Ingredient(5,3,false,"tomato"));
+    ingredients.put("gracehop", new Ingredient(4, ));
+    **/
+
+
+
+
 }
