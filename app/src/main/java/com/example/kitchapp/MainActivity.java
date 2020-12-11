6233e6228ec540449e1a9bd5f453efe0edfc438b
gulcin-ozkahya <g.ozkahya@ug.bilkent.edu.tr>
2020-12-11 21:39:09 +0300

    Merge branch 'main' of https://github.com/AliGenis/Kitchapp into main
    # Conflicts:
    #	app/src/main/java/com/example/kitchapp/MainActivity.java

--- app/src/main/java/com/example/kitchapp/MainActivity.java
+++ app/src/main/java/com/example/kitchapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kitchapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
