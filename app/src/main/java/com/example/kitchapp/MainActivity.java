639f0af6b3bda1083fd76928f6909b5bb65690d6
AliGenis <72208412+AliGenis@users.noreply.github.com>
2020-12-12 23:53:49 +0300

    Bottom Nav Menu added
    Also empty screens created for blueprint purposes to create navigation menu

--- app/src/main/java/com/example/kitchapp/MainActivity.java
+++ app/src/main/java/com/example/kitchapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kitchapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.gms.common.util.CollectionUtils;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.internal.NavigationMenu;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import android.content.Intent;
@@ -16,23 +29,50 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        /**setContentView(R.layout.fragment_suggestion_screen);
+
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.fragment_suggestion_screen);
+
+        setContentView(R.layout.activity_real_main);
+        BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+
+
+        //FIXME:app crashes when this two lines tries to make menu usable
+        //FIXME:it is related with NavController
+        //NavController navController = getNavController();
+        //NavigationUI.setupWithNavController(navView,navController);
+
+        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration()
+        //setContentView(R.layout.fragment_suggestion_screen);
+
         System.out.println("KITCHAPP!");
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!"); **/
+        myRef.setValue("Hello, World!");
+
 
         button = (Button)findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener(){
+        /*button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 openDataInsert();
-            }});
+            }});*/
+
+
     }
+    @NonNull
+    private  NavController getNavController() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fridgeFragment);
+        if (!(fragment instanceof NavHostFragment)) {
+            throw new IllegalStateException("Activity " + this
+                    + " does not have a NavHostFragment");
+        }
+        return ((NavHostFragment) fragment).getNavController();
+
+    }
+
     public void openDataInsert(){
         Intent intent = new Intent(this,DataInsert.class);
         startActivity(intent);
     }
-
 }
--- app/src/main/java/com/example/kitchapp/SuggestionScreen.java
+++ app/src/main/java/com/example/kitchapp/SuggestionScreen.java
@@ -12,21 +12,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SuggestionScreen#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SuggestionScreen extends Fragment {
+public class SuggestionScreen extends AppCompatActivity{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //private static final String ARG_PARAM1 = "param1";
+    //private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
+    /*private String mParam1;
     private String mParam2;
     TabHost tab;
 
@@ -42,6 +43,7 @@ public class SuggestionScreen extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment SuggestionScreen.
      */
+    /*
     // TODO: Rename and change types and number of parameters
     public static SuggestionScreen newInstance(String param1, String param2) {
         SuggestionScreen fragment = new SuggestionScreen();
@@ -50,13 +52,15 @@ public class SuggestionScreen extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
+    }*/
 
-    @SuppressLint("WrongViewCast")
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_suggestion_screen);
+        TextView textView = (TextView)findViewById(R.id.suggestedMealName);
+        textView.setText("Deneme");
         /*Trying to add functional tabs
         tab=(TabHost)findViewById(R.id.tabs);
         tab.setup();
@@ -70,16 +74,18 @@ public class SuggestionScreen extends Fragment {
         tab.addTab(s1);
         tab.addTab(s2);
         */
+        /*
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        }*/
     }
 
+    /*
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_suggestion_screen, container, false);
-    }
+    }*/
 }
