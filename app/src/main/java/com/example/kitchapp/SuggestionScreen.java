f54eb0cb546aa6a28a2807eaf0269ed04b4efaa5
AliGenis <72208412+AliGenis@users.noreply.github.com>
2020-12-23 23:34:02 +0300

    Suggestion UI minor bug fixes
    Co-Authored-By: tarikdemiroren <72218504+tarikdemiroren@users.noreply.github.com>

--- app/src/main/java/com/example/kitchapp/SuggestionScreen.java
+++ app/src/main/java/com/example/kitchapp/SuggestionScreen.java
@@ -3,6 +3,8 @@ package com.example.kitchapp;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -86,11 +88,18 @@ public class SuggestionScreen extends Fragment{
         }*/
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView timeText = view.findViewById(R.id.timeText);
+        timeText.setText("20min");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_suggestion_screen, container, false);
         //TextView textView = textView.findViewById(R.id.timeText);
         //textView.setText("Deneme");
