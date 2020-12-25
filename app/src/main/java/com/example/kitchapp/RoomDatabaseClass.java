1e1aeaaee3426b6b8e9dc8594d9a22246f2114c0
AliGenis <72208412+AliGenis@users.noreply.github.com>
2020-12-25 15:41:43 +0300

    Update RoomDatabaseClass.java

--- app/src/main/java/com/example/kitchapp/RoomDatabaseClass.java
+++ app/src/main/java/com/example/kitchapp/RoomDatabaseClass.java
@@ -1,7 +1,9 @@
 package com.example.kitchapp;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {Ingredient.class}, version = 2)
 public abstract class RoomDatabaseClass extends RoomDatabase {
     public abstract Ingredient ingredientDao();
 }
