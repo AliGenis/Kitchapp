0ad001768de6ee762dbeb533d53d3c2824a64431
Ömer Burak Doğan <omerburakdogan16@gmail.com>
2020-12-25 23:16:10 +0300

    fixed bugs, at least it is runnable and all fragments are visible

--- app/src/main/java/com/example/kitchapp/ui/recipes/RecipesFragment.java
+++ app/src/main/java/com/example/kitchapp/ui/recipes/RecipesFragment.java
@@ -1,59 +1,39 @@
 package com.example.kitchapp.ui.recipes;
 
-/**
- * A fragment representing a list of Items.
- */
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.kitchapp.Ingredient;
 import com.example.kitchapp.MainActivity;
 import com.example.kitchapp.R;
+import com.example.kitchapp.Recipe;
 
 import java.util.List;
-
 
 public class RecipesFragment extends Fragment {
 
     RecyclerAdapter recyclerAdapter;
-    Button btAddData;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_fridge, container, false);
+        View view = inflater.inflate(R.layout.fragment_recipes, container, false);
 
-        recyclerView = view.findViewById(R.id.fridgeRecyclerView);
+        recyclerView = view.findViewById(R.id.recipesRecyclerView);
         layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
-        List<Ingredient> ingredientList = MainActivity.roomDatabaseClass.ingredientDao().getIngredient();
-        recyclerAdapter = new RecyclerAdapter(ingredientList);
+        List<Recipe> recipeList = MainActivity.roomDatabaseClass.recipeDao().getRecipe();
+        recyclerAdapter = new RecyclerAdapter(recipeList);
         recyclerView.setAdapter(recyclerAdapter);
 
-        btAddData = view.findViewById(R.id.addToFridgeButton);
-        btAddData.setOnClickListener(this);
-
         return view;
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case (R.id.addToFridgeButton):
-                MainActivity.fragmentManager.beginTransaction().replace(R.id.Container, new AddDataFragment()
-                        , null).addToBackStack(null).commit();
-                break;
-        }
     }
 }
 
--- app/src/main/java/com/example/kitchapp/ui/shoppinglist/ShoppingListFragment.java
+++ app/src/main/java/com/example/kitchapp/ui/shoppinglist/ShoppingListFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,41 +16,23 @@ import com.example.kitchapp.R;
 
 import java.util.List;
 
-/**
- * A fragment representing a list of Items.
- */
 public class ShoppingListFragment extends Fragment {
 
     MyShoppingListRecyclerViewAdapter recyclerAdapter;
-    Button btAddData;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_fridge, container, false);
+        View view = inflater.inflate(R.layout.fragment_shopping_item_list, container, false);
 
         recyclerView = view.findViewById(R.id.list);
-        layoutManager = new LinearLayoutManager(getActivity());//
-        //recyclerView.setLayoutManager(layoutManager);
-        List<Ingredient> ingredientList = MainActivity.roomDatabaseClass.shoppingIngredientDao().getIngredient();
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        List<Ingredient> ingredientList = MainActivity.roomDatabaseClass.ingredientDao().getIngredient();
         recyclerAdapter = new MyShoppingListRecyclerViewAdapter(ingredientList);
-        //recyclerView.setAdapter(recyclerAdapter);
-
-        //btAddData = view.findViewById(R.id.addToFridgeButton);
-        //btAddData.setOnClickListener(this);
+        recyclerView.setAdapter(recyclerAdapter);
 
         return view;
     }
-    /*
-    @Override
-    public void onClick(View v) {
-        switch (v.getId())
-        {
-            case (R.id.addToFridgeButton):
-                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container, new AddDataFragment()
-                        , null).addToBackStack(null).commit();
-                break;
-        }
-    }*/
 }
