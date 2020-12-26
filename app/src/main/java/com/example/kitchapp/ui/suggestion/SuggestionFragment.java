e2d06d86edd7b1caccc67aa10dc4f86e870a1eea
gulcin-ozkahya <g.ozkahya@ug.bilkent.edu.tr>
2020-12-26 20:17:47 +0300

    Revert "Changes on the SuggestionFragment 'main' of https://github.com/AliGenis/Kitchapp into main"
    This reverts commit b11b1aaa21467c32580388e846ddb15ab7964887.

--- app/src/main/java/com/example/kitchapp/ui/suggestion/SuggestionFragment.java
+++ app/src/main/java/com/example/kitchapp/ui/suggestion/SuggestionFragment.java
@@ -9,17 +9,11 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.kitchapp.MainActivity;
 import com.example.kitchapp.R;
 import com.example.kitchapp.Recipe;
-import com.example.kitchapp.ui.recipes.AddRecipeFragment;
-import com.example.kitchapp.ui.recipes.OneRecipeFragment;
-import com.example.kitchapp.ui.recipes.RecyclerAdapter;
 import com.example.kitchapp.ui.suggestion.tabs.IngredientsFragment;
 import com.example.kitchapp.ui.suggestion.tabs.RecipeFragment;
 import com.google.android.material.tabs.TabLayout;
@@ -27,12 +21,12 @@ import com.google.android.material.tabs.TabLayout;
 import java.util.List;
 
 public class SuggestionFragment extends Fragment {
+
     ViewPager viewPager;
     TabLayout tabLayout;
     ImageButton btAccept, btReject;
     TextView recipeTitle, recipeInfo, calorieInfo, prepTime;
     List<Recipe> suggestionList;
-    private RecyclerView.LayoutManager layoutManager;
 
     public SuggestionFragment() {
     }
@@ -45,8 +39,6 @@ public class SuggestionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_suggestion, container, false);
-        List<Recipe> recipeList = MainActivity.roomDatabaseClass.recipeDao().getRecipe();
-
 
         viewPager = view.findViewById(R.id.viewPager);
         tabLayout = view.findViewById(R.id.tabLayout);
@@ -57,7 +49,7 @@ public class SuggestionFragment extends Fragment {
         calorieInfo = view.findViewById(R.id.calorieInfo);
         prepTime = view.findViewById(R.id.prepTimeInfo);
 
-        btReject.setOnClickListener(new View.OnClickListener() {
+        btAccept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Recipe suggestion;
@@ -65,38 +57,18 @@ public class SuggestionFragment extends Fragment {
                     suggestion = suggestionList.get(0);
                     suggestionList.remove(0);
                     recipeTitle.setText(suggestion.getName());
-                    calorieInfo.setText(Double.toString(suggestion.getCalorie()));
-                    prepTime.setText(Integer.toString(suggestion.getPrepTime()));
                 } else{
                     recipeTitle.setText("No suggestion.");
-                }
-            }
-        });
-        btAccept.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case (R.id.acceptButton):
-                      Fragment oneRecipe = new OneRecipeFragment();
-                        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                        fragmentTransaction.replace(R.id.Container,oneRecipe);
-                        fragmentTransaction.addToBackStack(null);
-                        fragmentTransaction.commit();
-                 //       MainActivity.fragmentManager.beginTransaction().replace(R.id.Container, new OneRecipeFragment()
-                     //           , null).addToBackStack(null).commit();
-                      break;
                 }
             }
         });
 
         suggestionList = MainActivity.roomDatabaseClass.recipeDao().getRecipe();
 
-
         return view;
     }
 
-  @Override
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
