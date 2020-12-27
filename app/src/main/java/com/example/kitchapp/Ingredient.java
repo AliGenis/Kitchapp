4b48b94f8dcfe33147f4dcf815e0455e6310b86c
tarikdemiroren <tarekarez5652@gmail.com>
2020-12-27 14:31:21 +0300

    Commentation

--- app/src/main/java/com/example/kitchapp/Ingredient.java
+++ app/src/main/java/com/example/kitchapp/Ingredient.java
@@ -34,50 +34,97 @@ public class Ingredient {
         this.number = number;
     }
 
+    /**
+     * get method for id
+     * @return int id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * set method for id property
+     * @param id takes an int value and set the id to it.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * get method for name property
+     * @return String name
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * set method for name property
+     * @param name takes a String type variable from outside and sets the name to it
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * get method for number property
+     * @return int number
+     */
     public int getNumber() {
         return number;
     }
 
+    /**
+     * set method for number property
+     * @param number takes an int value from outside and sets the number to it.
+     */
     public void setNumber(int number) {
         this.number = number;
     }
 
+    /**
+     * get method for defaultBuyValue property
+     * @return int defaultBuyValue
+     */
     public int getDefaultBuyValue() {
         return defaultBuyValue;
     }
 
+    /**
+     * set method for defaultBuyValue property
+     * @param defaultBuyValue takes an int value from outside and sets the defaultBuyValue to it.
+     */
     public void setDefaultBuyValue(int defaultBuyValue) {
         this.defaultBuyValue = defaultBuyValue;
     }
 
+    /**
+     * check method for the ingredient object to see whether Fridge contains the Object
+     * @return boolean isInFridge
+     */
     public boolean isInFridge() {
         return isInFridge;
     }
 
+    /**
+     * set method for isInFridge property
+     * @param inFridge takes a boolean value and set the isInFridge to it.
+     */
     public void setInFridge(boolean inFridge) {
         isInFridge = inFridge;
     }
 
+    /**
+     * check method for the ingredient object to see whether ShoppingList contains the Object
+     * @return boolean isInShoppingList
+     */
     public boolean isInShoppingList() {
         return isInShoppingList;
     }
 
+    /**
+     * set method for property isInShoppingList
+     * @param inShoppingList takes a boolean value and set the isInShoppingList to it.
+     */
     public void setInShoppingList(boolean inShoppingList) {
         isInShoppingList = inShoppingList;
     }
--- app/src/main/java/com/example/kitchapp/Recipe.java
+++ app/src/main/java/com/example/kitchapp/Recipe.java
@@ -30,76 +30,132 @@ public class Recipe {
     private double calorie;
     @ColumnInfo(name = "prepTime")
     private int prepTime;
-    @ColumnInfo(name = "recipePhotoId")
-    private int recipePhotoId;
+    //@ColumnInfo(name = "recipePhotoId")
+    //private int recipePhotoId;
     @ColumnInfo(name = "ingredients")
     public String ingredients;
 
+    /**
+     * get method for ingredients property
+     * @return String ingredients
+     */
     public String getIngredients() {
         return ingredients;
     }
 
+    /**
+     * set method for ingredients property
+     * @param ingredients
+     */
     public void setIngredients(String ingredients) {
         this.ingredients = ingredients;
     }
 
+    /**
+     * @return List<Ingredients> returns list of ingredients provided by the string type property of ingredients
+     *                           by translating the ingredients to List type property
+     */
     public List<Ingredient> getIngredientList() {
         Type listType = new TypeToken<ArrayList<Ingredient>>(){}.getType();
         List<Ingredient> list = new Gson().fromJson(ingredients, listType);
         return list;
     }
 
+    /**
+     * translates this List type property to a string type variable and sets the ingredients to it.
+     * @param ingredients List type property
+     */
     public void setIngredientList(List<Ingredient> ingredients) {
         Gson gson = new Gson();
         String jsonString = gson.toJson(ingredients);
         this.ingredients = jsonString;
     }
 
+    /**
+     * get method for recipeID property
+     * @return int recipeID
+     */
     public int getRecipeID() {
         return recipeID;
     }
 
+    /**
+     * set method for recipeID property
+     * @param recipeID takes an int from outside and sets the recipeID to it.
+     */
     public void setRecipeID(int recipeID) {
         this.recipeID = recipeID;
     }
 
+    /**
+     * get method for the name property
+     * @return String name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * set method for the name property
+     * @param name takes a string from outside and sets the name to it.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * get method for the recipe property
+     * @return String Recipe
+     */
     public String getRecipe() {
         return recipe;
     }
-
+    /**
+     * set method for the recipe property
+     * @param recipe takes a string from outside and sets the recipe
+     */
     public void setRecipe(String recipe) {
         this.recipe = recipe;
     }
 
+    /**
+     * get mathod for the calorie
+     * @return double calorie
+     */
     public double getCalorie() {
         return calorie;
     }
 
+    /**
+     * set method for calorie property
+     * @param calorie takes a double value as a parameter and sets calorie to it
+     */
     public void setCalorie(double calorie) {
         this.calorie = calorie;
     }
 
+    /**
+     * get method for prepTime property
+     * @return int prepTime
+     */
     public int getPrepTime() {
         return prepTime;
     }
 
+    /**
+     * set method for prepTime property
+     * @param prepTime takes a int value from outside and sets the prepTime to it.
+     */
     public void setPrepTime(int prepTime) {
         this.prepTime = prepTime;
     }
 
+    /*
     public int getRecipePhotoId() {
         return recipePhotoId;
     }
 
     public void setRecipePhotoId(int recipePhotoId) {
         this.recipePhotoId = recipePhotoId;
-    }
+    }*/
 }
